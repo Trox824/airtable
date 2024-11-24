cf0058151c80bb2697ac35fe01c6c4ca5e43a858
Trox824 <nthung2k4@gmail.com>
2024-11-24 10:50:51 +0700

    fix auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,7 @@
 import NextAuth from "next-auth";
-import { NextApiRequest, NextApiResponse } from "next";
+
 import { authOptions } from "~/server/auth";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
-
-// Add a new handler for logout
-export const logoutHandler = async (
-  req: NextApiRequest,
-  res: NextApiResponse,
-) => {
-  await NextAuth(req, res, { ...authOptions, pages: { signOut: "/logout" } });
-};
-
-// Export the logout handler
-export { logoutHandler as DELETE };
