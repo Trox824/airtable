d4e303120b028216e511fb2bd6b64733ff70baac
Trox824 <118362262+Trox824@users.noreply.github.com>
2025-01-08 11:41:34 +0700

    Revert "Fix: change package"
    This reverts commit bf258ca08e0119aab966b3bb270db04c0bbf2846.

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,14 +7,11 @@ import {
 } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { db } from "./db";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { compare } from "bcrypt";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      role: "USER" | "ADMIN";
     } & DefaultSession["user"];
   }
 }
@@ -26,62 +23,17 @@ export const authOptions: NextAuthOptions = {
       user: {
         ...session.user,
         id: token.id,
-        role: token.role,
       },
     }),
     jwt: ({ token, user }) => {
       if (user) {
         token.id = user.id;
-        token.role = user.role;
       }
       return token;
     },
   },
   adapter: PrismaAdapter(db),
   providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Invalid credentials");
-        }
-
-        const user = await db.user.findUnique({
-          where: { email: credentials.email },
-          select: {
-            id: true,
-            email: true,
-            name: true,
-            password: true,
-            role: true,
-          },
-        });
-
-        if (!user || !user.password) {
-          throw new Error("Invalid credentials");
-        }
-
-        const isPasswordValid = await compare(
-          credentials.password,
-          user.password,
-        );
-
-        if (!isPasswordValid) {
-          throw new Error("Invalid credentials");
-        }
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          role: user.role,
-        };
-      },
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
